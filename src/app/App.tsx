3e59b6fc01895da6173d10f23d98f4568720d143
evn.sibirev <evn.sibirev@stc-spb.ru>
2024-04-09 09:04:22 +0300

    added applink shared ui component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,17 @@
-import { Route, Routes } from 'react-router-dom';
 import './styles/index.scss';
-import { Link } from 'react-router-dom';
-import { Suspense } from 'react';
 import { useTheme } from './providers/ThemeProvider/lib/useTheme';
 import { classNames } from '../shared/lib/classNames/classNames';
-import { MainPage } from 'pages/MainPage';
-import { AboutPage } from 'pages/AboutPage';
 import { AppRouter } from './providers/router';
+import { Navbar } from 'widgets/Navbar';
 
 const App = () => {
 	const {theme, toggleTheme } = useTheme();
 	
 	return (
 		<div className={classNames('app', {}, [theme])}>
-			<button onClick={toggleTheme}>toggle theme</button>
-			<Link to='/'>main</Link>
-			<Link to='/about'>about</Link>
+			<Navbar />
 			<AppRouter />
+			<button onClick={toggleTheme}>toggle theme</button>
 		</div>
 	);
 };
