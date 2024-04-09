b1061c24160622f2e3e489ca2fde9ede20235c54
evn.sibirev <evn.sibirev@stc-spb.ru>
2024-04-09 08:16:42 +0300

    added route config + approuter component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,6 +6,7 @@ import { useTheme } from './providers/ThemeProvider/lib/useTheme';
 import { classNames } from '../shared/lib/classNames/classNames';
 import { MainPage } from 'pages/MainPage';
 import { AboutPage } from 'pages/AboutPage';
+import { AppRouter } from './providers/router';
 
 const App = () => {
 	const {theme, toggleTheme } = useTheme();
@@ -15,12 +16,7 @@ const App = () => {
 			<button onClick={toggleTheme}>toggle theme</button>
 			<Link to='/'>main</Link>
 			<Link to='/about'>about</Link>
-			<Suspense fallback={<div>Loading...</div>}>
-				<Routes>
-						<Route path={'/'} element={<MainPage />} />
-						<Route path={'/about'} element={<AboutPage />} />
-				</Routes>
-			</Suspense>
+			<AppRouter />
 		</div>
 	);
 };
