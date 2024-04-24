85b622e16bba63c90a88541246d29988e2bccc45
eunicsi <seugeny97@gmail.com>
2024-04-24 11:17:08 +0300

    added more tests and test async thunk class

---
+++ src/features/AuthByUsername/model/slice/loginSlice.test.ts
@@ -0,0 +1,25 @@
+import { loginByUsername } from '../services/loginByUsername/loginByUsername';
+import { LoginSchema } from '../types/loginSchema';
+import { loginActions, loginReducer } from './loginSlice';
+
+describe('loginSlice.test', () => {
+	test('test set username', async () => {
+		const state: DeepPartial<LoginSchema> = { username: '123' };
+		expect(
+			loginReducer(
+				state as LoginSchema,
+				loginActions.setUsername('123123'),
+			),
+		).toEqual({ username: '123123' });
+	});
+
+	test('test set password', async () => {
+		const state: DeepPartial<LoginSchema> = { password: '123' };
+		expect(
+			loginReducer(
+				state as LoginSchema,
+				loginActions.setPassword('123123'),
+			),
+		).toEqual({ password: '123123' });
+	});
+});
